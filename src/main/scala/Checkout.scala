1e48e32f788fa1e46104c2a27388df65ec501a06
James Rees <jamesrees94@live.co.uk>
2021-03-10 18:11:02 +0000

    Step 2 implementation

--- src/main/scala/Checkout.scala
+++ src/main/scala/Checkout.scala
@@ -5,7 +5,15 @@ class Checkout(){
     "Orange" -> 0.25
   )
 
+
+  private val offerDeductions: List[List[String] => BigDecimal] = List(
+    basket => Math.floor(basket.count(item => item.equals("Apple")) / 2.0) * prices.getOrElse[BigDecimal]("Apple", 0.0),
+    basket => Math.floor(basket.count(item => item.equals("Orange")) / 3.0) * prices.getOrElse[BigDecimal]("Orange", 0.0)
+  )
+
   def checkout(basket: List[String]): String = {
-    s"£${basket.flatMap(prices.get).sum.setScale(2)}"
+    val basketTotal = basket.flatMap(prices.get).sum
+    val deductions = offerDeductions.foldRight[BigDecimal](0.0)((offer, value) => offer(basket) + value)
+    s"£${(basketTotal - deductions).setScale(2)}"
   }
 }

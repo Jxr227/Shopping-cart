10c83f4d84c1d379d2ecf3a92f3948a7ba047a98
James Rees <jamesrees94@live.co.uk>
2021-03-10 18:07:19 +0000

    Tests for step 2

--- src/test/scala/CheckoutSpec.scala
+++ src/test/scala/CheckoutSpec.scala
@@ -31,8 +31,20 @@ class CheckoutSpec extends AnyWordSpec with should.Matchers{
       result should be("£0.85")
     }
 
-    "return £1.20 for a basket containing two apples" in {
+    "return £0.60 for a basket containing two apples" in {
       val result = checkout.checkout(List("Apple", "Apple"))
+
+      result should be("£0.60")
+    }
+
+    "return £1.20 for a basket containing three apples" in {
+      val result = checkout.checkout(List("Apple", "Apple", "Apple"))
+
+      result should be("£1.20")
+    }
+
+    "return £1.20 for a basket containing four apples" in {
+      val result = checkout.checkout(List("Apple", "Apple", "Apple", "Apple"))
 
       result should be("£1.20")
     }
@@ -43,16 +55,40 @@ class CheckoutSpec extends AnyWordSpec with should.Matchers{
       result should be("£0.50")
     }
 
-    "return £60.00 for a basket containing 100 apples" in {
-      val result = checkout.checkout(List.fill(100)("Apple"))
+    "return £0.50 for a basket containing three oranges" in {
+      val result = checkout.checkout(List("Orange", "Orange", "Orange"))
 
-      result should be("£60.00")
+      result should be("£0.50")
     }
 
-    "return £25.00 for a basket containing 100 oranges" in {
+    "return £0.75 for a basket containing four oranges" in {
+      val result = checkout.checkout(List("Orange", "Orange", "Orange", "Orange"))
+
+      result should be("£0.75")
+    }
+
+    "return £1.00 for a basket containing six oranges" in {
+      val result = checkout.checkout(List("Orange", "Orange", "Orange", "Orange", "Orange", "Orange"))
+
+      result should be("£1.00")
+    }
+
+    "return £30.00 for a basket containing 100 apples" in {
+      val result = checkout.checkout(List.fill(100)("Apple"))
+
+      result should be("£30.00")
+    }
+
+    "return £16.75 for a basket containing 100 oranges" in {
       val result = checkout.checkout(List.fill(100)("Orange"))
 
-      result should be("£25.00")
+      result should be("£16.75")
+    }
+
+    "return £46.75 for a basket containing 100 apples and 100 oranges" in {
+      val result = checkout.checkout(List.fill(100)("Orange") ++ List.fill(100)("Apple"))
+
+      result should be("£46.75")
     }
 
   }
